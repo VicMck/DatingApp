37129d40e1f9a8ece5532793a7a3020ff25e7953
VicMcklein <sorcervic@gmail.com>
2020-06-08 11:35:47 +0200

    Last update

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,18 +11,48 @@ import { AlertifyService } from '../_services/alertify.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(private authService: AuthService, 
+    private alertify: AlertifyService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createRegisterForm();
+    // FormGroup way to do it
+/*     this.registerForm = new FormGroup ({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', Validators.required)
+    }); */
+  }
+
+  createRegisterForm() {
+    // registerForm way to do it
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required]
+    }, {validator: this.passwordMatchValidator});
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    // tslint:disable-next-line: object-literal-key-quotes
+    return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch': true};
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
+/*     this.authService.register(this.model).subscribe(() => {
       this.alertify.success('registration successful');
     }, error => {
       this.alertify.error(error);
-    });
+    }); */
+    console.log(this.registerForm.value);
   }
 
   cancel() {
